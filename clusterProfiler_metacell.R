9348f46e39295f2e821310df616f89df479b55b3
cadorenathan <114237064+cadorenathan@users.noreply.github.com>
2023-05-03 15:20:02 -0300

    Add files via upload

---
+++ clusterProfiler_metacell.R
@@ -0,0 +1,128 @@
+setwd("C:/Users/cador/OneDrive/Doutorado/Meta SBGM/Meta_cell_SVA")
+getwd()
+
+library(dplyr)
+
+genes <- read.csv("meta_result_cell.csv", header=T)
+genes <- filter(genes, metap <= 0.05, metafc <= -1 | metafc >= 1)
+
+# BiocManager::install("DOSE")
+# BiocManager::install("clusterProfiler")
+# BiocManager::install("org.Hs.eg.db")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("KEGGREST", force = TRUE)
+# BiocManager::install("ReactomePA")
+
+# Load packages for functional enrichment
+
+library("DOSE")
+library("clusterProfiler")
+library("org.Hs.eg.db") # for ids mapping
+library("KEGGREST")
+
+genes.map <-select(org.Hs.eg.db, as.character(genes$SYMBOL),c("SYMBOL","ENTREZID"), "SYMBOL")
+colnames(genes)[1] <- "SYMBOL"
+genes.map <- merge(genes, genes.map)
+
+setwd("C:/Users/cador/OneDrive/Doutorado/Meta SBGM/clusterProfileR/Utilizando apenas significativos")
+getwd()
+
+genes.map <- read.csv("EntrezID_metacell.csv", header=T)
+
+## perform enrichment for all target genes found (using KEGG.db)
+enrich.kegg <- enrichKEGG(gene = unique(na.omit(genes.map$ENTREZID)),
+                          organism     = "human",
+                          pvalueCutoff = 0.1,
+                          use_internal_data = FALSE,
+                          pAdjustMethod = "fdr")
+# não consegui fazer o download do pacote KEGG.db então troquei o use_internal_data para FALSE
+
+enrich.kegg <- setReadable(enrich.kegg,org.Hs.eg.db,keyType = "ENTREZID")
+kegg.table <- as.data.frame(enrich.kegg)
+View(kegg.table)
+write.csv(kegg.table, "KEGG_meta_metacell.csv")
+
+# browseKEGG(enrich.kegg, 'hsa04060')
+
+## perform enrichment for all target genes found (using GO)
+ego <- enrichGO(gene          = unique(na.omit(genes.map$ENTREZID)),
+                OrgDb         = org.Hs.eg.db,
+                ont           = "BP",
+                pAdjustMethod = "fdr",
+                pvalueCutoff  = 0.05)
+enrich.go<- setReadable(ego,org.Hs.eg.db,keyType = "ENTREZID")
+go.table <- as.data.frame(enrich.go)
+
+#drop GO level 4
+ego2<-dropGO(ego,level=6)
+
+ego2 <- setReadable(ego2, OrgDb = org.Hs.eg.db)
+go2.table <- as.data.frame(ego2)
+View(go2.table)
+
+write.csv(go.table, "GO_targets_metacell.csv")
+write.csv(go2.table, "GO2_table_limb_HPO_metacell.csv")
+
+jpeg(file="dotplot_KEGG_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+dotplot(enrich.kegg, showCategory=20, orderBy="GeneRatio")
+dev.off()
+
+jpeg(file="dotplot_targets_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+dotplot(ego, showCategory=20)
+dev.off()
+enrichMap(ego)
+
+jpeg(file="cnetplot_GO_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+cnetplot(ego, categorySize="pvalue", showCategory = 3, fixed = TRUE, node_label='all')
+dev.off()
+
+##############################################################################
+
+library("ReactomePA")
+
+rPA <- enrichPathway(gene=genes.map$ENTREZID,pvalueCutoff=0.1, readable=T)
+rPA2 <- as.data.frame(rPA)
+View(rPA2)
+write.csv(rPA2, "Reactome_metacell.csv")
+
+jpeg(file="dotplot_Reactome_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+dotplot(rPA, showCategory=20)
+dev.off()
+
+
+#####################################################
+
+genes.code <- genes.map$ENTREZID
+
+#enrich disease ontology
+enrich.dis <- enrichDO(gene          = genes.code,
+              ont           = "DO",
+              pvalueCutoff  = 0.05,
+              pAdjustMethod = "BH",
+              universe      = names(genes.code),
+              minGSSize     = 5,
+              maxGSSize     = 500,
+              qvalueCutoff  = 0.05,
+              readable      = FALSE)
+
+disease.read <- setReadable(enrich.dis,'org.Hs.eg.db')
+disease.table <- disease.read@result
+
+
+#enrich disGenNet
+dgn <- enrichDGN(genes.map$ENTREZID)
+dgn.read <- setReadable(dgn,'org.Hs.eg.db')
+dgn.table <- dgn.read@result
+
+#plots
+library(enrichplot)
+
+jpeg(file="dotplot_DGN_metacell.jpeg", width=8200, height=4800, units="px", res=600)
+dotplot(dgn, showCategory=20)
+dev.off()
+
+write.csv(dgn.table, "dgn_metacell.csv")
+
+########################################################
+
