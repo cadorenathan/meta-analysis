0d4f274a261108d51ddc6592a8174a08761bd4f0
cadorenathan <114237064+cadorenathan@users.noreply.github.com>
2023-05-03 15:30:29 -0300

    Update clusterProfiler_metacell.R

--- clusterProfiler_metacell.R
+++ clusterProfiler_metacell.R
@@ -1,17 +1,7 @@
-setwd("C:/Users/cador/OneDrive/Doutorado/Meta SBGM/Meta_cell_SVA")
-getwd()
-
-library(dplyr)
-
-genes <- read.csv("meta_result_cell.csv", header=T)
-genes <- filter(genes, metap <= 0.05, metafc <= -1 | metafc >= 1)
-
 # BiocManager::install("DOSE")
 # BiocManager::install("clusterProfiler")
 # BiocManager::install("org.Hs.eg.db")
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# BiocManager::install("KEGGREST", force = TRUE)
+# BiocManager::install("KEGGREST")
 # BiocManager::install("ReactomePA")
 
 # Load packages for functional enrichment
@@ -21,29 +11,7 @@ library("clusterProfiler")
 library("org.Hs.eg.db") # for ids mapping
 library("KEGGREST")
 
-genes.map <-select(org.Hs.eg.db, as.character(genes$SYMBOL),c("SYMBOL","ENTREZID"), "SYMBOL")
-colnames(genes)[1] <- "SYMBOL"
-genes.map <- merge(genes, genes.map)
-
-setwd("C:/Users/cador/OneDrive/Doutorado/Meta SBGM/clusterProfileR/Utilizando apenas significativos")
-getwd()
-
-genes.map <- read.csv("EntrezID_metacell.csv", header=T)
-
-## perform enrichment for all target genes found (using KEGG.db)
-enrich.kegg <- enrichKEGG(gene = unique(na.omit(genes.map$ENTREZID)),
-                          organism     = "human",
-                          pvalueCutoff = 0.1,
-                          use_internal_data = FALSE,
-                          pAdjustMethod = "fdr")
-# não consegui fazer o download do pacote KEGG.db então troquei o use_internal_data para FALSE
-
-enrich.kegg <- setReadable(enrich.kegg,org.Hs.eg.db,keyType = "ENTREZID")
-kegg.table <- as.data.frame(enrich.kegg)
-View(kegg.table)
-write.csv(kegg.table, "KEGG_meta_metacell.csv")
-
-# browseKEGG(enrich.kegg, 'hsa04060')
+# load your gene list and atribute to "genes.map" 
 
 ## perform enrichment for all target genes found (using GO)
 ego <- enrichGO(gene          = unique(na.omit(genes.map$ENTREZID)),
@@ -61,19 +29,19 @@ ego2 <- setReadable(ego2, OrgDb = org.Hs.eg.db)
 go2.table <- as.data.frame(ego2)
 View(go2.table)
 
-write.csv(go.table, "GO_targets_metacell.csv")
-write.csv(go2.table, "GO2_table_limb_HPO_metacell.csv")
+write.csv(go.table, "GO_targets.csv")
+write.csv(go2.table, "GO2_table.csv")
 
-jpeg(file="dotplot_KEGG_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+jpeg(file="dotplot_KEGG.jpeg", width=6200, height=4800, units="px", res=600)
 dotplot(enrich.kegg, showCategory=20, orderBy="GeneRatio")
 dev.off()
 
-jpeg(file="dotplot_targets_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+jpeg(file="dotplot_targets.jpeg", width=6200, height=4800, units="px", res=600)
 dotplot(ego, showCategory=20)
 dev.off()
 enrichMap(ego)
 
-jpeg(file="cnetplot_GO_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+jpeg(file="cnetplot_GO.jpeg", width=6200, height=4800, units="px", res=600)
 cnetplot(ego, categorySize="pvalue", showCategory = 3, fixed = TRUE, node_label='all')
 dev.off()
 
@@ -84,9 +52,9 @@ library("ReactomePA")
 rPA <- enrichPathway(gene=genes.map$ENTREZID,pvalueCutoff=0.1, readable=T)
 rPA2 <- as.data.frame(rPA)
 View(rPA2)
-write.csv(rPA2, "Reactome_metacell.csv")
+write.csv(rPA2, "Reactome_results.csv")
 
-jpeg(file="dotplot_Reactome_metacell.jpeg", width=6200, height=4800, units="px", res=600)
+jpeg(file="dotplot_Reactome.jpeg", width=6200, height=4800, units="px", res=600)
 dotplot(rPA, showCategory=20)
 dev.off()
 
@@ -118,11 +86,26 @@ dgn.table <- dgn.read@result
 #plots
 library(enrichplot)
 
-jpeg(file="dotplot_DGN_metacell.jpeg", width=8200, height=4800, units="px", res=600)
+jpeg(file="dotplot_DGN.jpeg", width=8200, height=4800, units="px", res=600)
 dotplot(dgn, showCategory=20)
 dev.off()
 
-write.csv(dgn.table, "dgn_metacell.csv")
+write.csv(dgn.table, "dgn_results.csv")
 
 ########################################################
+
+# perform enrichment for all target genes found (using KEGG.db)
+enrich.kegg <- enrichKEGG(gene = unique(na.omit(genes.map$ENTREZID)),
+                          organism     = "human",
+                          pvalueCutoff = 0.1,
+                          use_internal_data = FALSE,
+                          pAdjustMethod = "fdr")
+
+enrich.kegg <- setReadable(enrich.kegg,org.Hs.eg.db,keyType = "ENTREZID")
+kegg.table <- as.data.frame(enrich.kegg)
+View(kegg.table)
+write.csv(kegg.table, "KEGGresults.csv")
+
+# browseKEGG(enrich.kegg, 'hsa04060')
+
 
